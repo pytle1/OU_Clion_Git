91ddb28df267bb3148b4b27ece4cf4ebcb0b4265
pytle1 <pytle@protonmail.com>
2021-02-05 20:50:06 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_CLion_Git.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_CLion_Git.dir/main.cpp.o"
+  "OU_CLion_Git"
+  "OU_CLion_Git.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU_CLion_Git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
